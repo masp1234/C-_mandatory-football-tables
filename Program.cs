42e1bf2f97c8acb12096ad952ee01ce93129e99c
masp1234 <mart2c04@stud.kea.dk>
2023-03-21 14:52:59 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Football_tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
